efd54013459830a9a3eaf2c3c5fbaef42c36dde9
CesarVenegasM <89415502+CesarVenegasM@users.noreply.github.com>
2021-12-06 21:51:16 -0800

    Update SumaVectores.s

--- Capitulo_2/SumaVectores.s
+++ Capitulo_2/SumaVectores.s
@@ -4,3 +4,19 @@
 @@ Autor: Venegas Mendoza Cesar Ivan
 @@ nickname: CesarVenegasM
 @@ Fecha de revision: 06/12/2021
+.data
+var1: .asciz  "La suma es %d\n"
+var2: .word 128, 32, 100, -30, 124
+
+.text
+.global main
+
+main: push  {r4, lr}
+
+  //Bucle para realizar la suma
+  
+    bne bucle //Nuevo llamado al bucle
+    //Aqui se imprime el resultado
+    
+    //Recuperamos registros y salimos
+    

a6f76002e3940d14c390db87705f333a4c0c784b
CesarVenegasM <89415502+CesarVenegasM@users.noreply.github.com>
2021-12-06 22:17:43 -0800

    Create Ciclos.s

---
+++ Capitulo_2/Ciclos.s
@@ -0,0 +1,6 @@
+@@ Instituto Tecnologico de Tijuana
+@@ Depto de Sistemas y Computacion
+@@ Ing. En Sistemas Computacionales
+@@ Autor: Venegas Mendoza Cesar Ivan
+@@ nickname: CesarVenegasM
+@@ Fecha de revision: 06/12/2021

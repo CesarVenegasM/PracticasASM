eff1b92729fbf2fc9477e7e4638ec4844136a814
CesarVenegasM <89415502+CesarVenegasM@users.noreply.github.com>
2021-12-09 11:51:44 -0800

    Create Subrutina2.s

---
+++ Capitulo_3/Subrutina2.s
@@ -0,0 +1,8 @@
+@@ Instituto Tecnologico de Tijuana
+@@ Depto de Sistemas y Computacion
+@@ Ing. En Sistemas Computacionales
+@@ Autor: Venegas Mendoza Cesar Ivan
+@@ nickname: CesarVenegasM
+@@ Fecha de revision: 09/12/2021
+
+@@Sección codigo

259802f2c0982c9928676457859dfe071b08bbb6
CesarVenegasM <89415502+CesarVenegasM@users.noreply.github.com>
2021-12-11 18:29:55 -0800

    Create Stack.s

---
+++ ProgramasSelectos/Stack.s
@@ -0,0 +1,15 @@
+@@ Instituto Tecnologico de Tijuana
+@@ Depto de Sistemas y Computacion
+@@ Ing. En Sistemas Computacionales
+@@ Autor: Venegas Mendoza Cesar Ivan
+@@ nickname: CesarVenegasM
+@@ Fecha de revision: 11/12/2021
+
+.global main
+
+main:
+     mov   r0, #2  
+     push  {r0}    
+     mov   r0, #3  
+     pop   {r0}    
+     bx    lr      

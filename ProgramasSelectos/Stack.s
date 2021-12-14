822a2304faa09a57a81cdec177ab2c63bdcdebee
CesarVenegasM <89415502+CesarVenegasM@users.noreply.github.com>
2021-12-13 17:36:42 -0800

    Update Stack.s

--- ProgramasSelectos/Stack.s
+++ ProgramasSelectos/Stack.s
@@ -8,8 +8,8 @@
 .global main
 
 main:
-     mov   r0, #2  
-     push  {r0}    
-     mov   r0, #3  
-     pop   {r0}    
-     bx    lr      
+     mov   r0, #2  //Inicializacion de r0
+     push  {r0}    //Guardar R0 en la pila
+     mov   r0, #3  // Guardar R0
+     pop   {r0}    // Restablecer R0 a su valor inicial
+     bx    lr      // Finalizar el programa
